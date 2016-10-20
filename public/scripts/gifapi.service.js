c24e3b860c8c464167cafaf76ea2896c2f9310f7
Vaughn Prosser <vaughnprosser@gmail.com>
2016-10-20 14:33:07 -0500

    Pokemon branch

--- public/scripts/gifapi.service.js
+++ public/scripts/gifapi.service.js
@@ -1,23 +1,37 @@
-angular.module('giphyApp')
-       .service('gifapi', GifAPIService);
+angular.module('pokemonApp')
+       .service('pokeapi', PokemonService);
 
-function GifAPIService($http) {
-  var API = 'http://api.giphy.com/v1/gifs';
-  var key = 'api_key=dc6zaTOxFJmzC';
-  this.getRandom = function () {
+function PokemonService($http) {
+  var API = 'http://pokeapi.co/api/v2';
+  this.getPokemon = function () {
 
-    return $http.get(API + '/random?' + key)
+    return $http.get(API + '/generation/1/')
         .then(function (response) {
-          return response.data.data;
+          console.log('response: ', response);
+          return response.data.pokemon_species;
         });
   };
 
-  this.getSearch = function (searchPhrase) {
+  // this.getSelectedPokemonText = function (name) {
+  //     return $http.get(API + '/pokemon-species/' + name)
+  //                 .then(function (response) {
+  //                   return response.data.flavor_text_entries;
+  //                 });
+  //   };
 
-    return $http.get(API + '/search?q=' + encodeURI(searchPhrase.q.replace(' ', '+')) + '&' + key)
-    .then(function (response) {
-      return response.data.data;
-    });
+  this.getSelectedPokemon = function (name) {
+    return $http.get(API + '/pokemon/' + name + '/')
+                .then(function (response) {
+                  return response.data;
+                });
   };
+
+  // this.getSearch = function (searchPhrase) {
+  //
+  //   return $http.get(API + '/search?q=' + encodeURI(searchPhrase.q.replace(' ', '+')) + '&' + key)
+  //   .then(function (response) {
+  //     return response.data.data;
+  //   });
+  // };
 
 }
--- public/scripts/main.controller.js
+++ public/scripts/main.controller.js
@@ -1,24 +1,45 @@
-angular.module('giphyApp')
+angular.module('pokemonApp')
        .controller('MainController', MainController);
 
-function MainController(gifapi) {
+function MainController(pokeapi) {
   var main = this;
   console.log('Maincontroller loaded');
 
-  main.gifs = {};
-  main.gifsArray = [];
-
-  main.getRandom = function () {
-    gifapi.getRandom().then(function (gif) {
-      main.gifs = gif;
-    });
-  };
-
-  main.getSearch = function () {
-    gifapi.getSearch(main.searchPhrase).then(function (list) {
-      main.gifsArray = list;
+  main.pokemonSelected = {};
+  main.pokemonArray = [];
+  main.flavorText = '';
+  pokeapi.getPokemon().then(function (poke) {
+    main.pokemonArray = poke;
+    main.pokemonArray.sort(function (a, b) {
+      if (a.name < b.name) return -1;
+      if (a.name > b.name) return 1;
+      return 0;
     });
 
+    console.log('Poke: ', poke);
+  });
+
+  main.getSelectedPokemon = function () {
+    pokeapi.getSelectedPokemon(main.selected)
+           .then(function (pokemon) {
+            console.log('main.selected: ', main.selected);
+            main.pokemonSelected = pokemon;
+            console.log('main.pokemonSelected: ', pokemon);
+          });
+
+    // pokeapi.getSelectedPokemonText(main.selected)
+    //        .then(function (flavorText) {
+    //         main.flavorText = flavorText;
+    //         console.log('flavorText: ', flavorText);
+    //       });
   };
+
+
+  // main.getSearch = function () {
+  //   gifapi.getSearch(main.searchPhrase).then(function (list) {
+  //     main.gifsArray = list;
+  //   });
+  //
+  // };
 
 };
